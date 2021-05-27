c7ec4ffcd247e6ac485c0a30c45ee1556b745c97
Younes Tamim <tami0007@algonquinlive.com>
2021-05-27 00:33:36 -0400

    Commit 6 widgets that use Text Module

--- app/src/main/java/com/example/younessandroidlabs/MainActivity.kt
+++ app/src/main/java/com/example/younessandroidlabs/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.younessandroidlabs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.textview)
+        var myedit = findViewById<EditText>(R.id.myedittext)
+        var editString = myedit.text
+        myedit.setText( "Your edit text has: $editString ")
+
+
+
+
     }
 }
